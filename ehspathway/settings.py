1c373eb1ade48d7da337f1bc4ae8ff9b52137852
KashCo26 <guptakashvi26@gmail.com>
2026-09-16 19:03:55 -0700

    added requirements.txt

--- ehspathway/settings.py
+++ ehspathway/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-84ryxq(1(^bg+bu_mxio@l7#on4rg&xnvw=nz#u=iw7$^k)qr+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',   
 ]
 
 ROOT_URLCONF = 'ehspathway.urls'
@@ -123,3 +124,5 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
+
+STATIC_ROOT = BASE_DIR/'staticfiles'
