7893ff2663dc4ec124d93e35df47f2a8c010f260
KashCo26 <guptakashvi26@gmail.com>
2026-09-18 22:48:45 -0700

    configure project for render deployment

--- ehspathway/settings.py
+++ ehspathway/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-84ryxq(1(^bg+bu_mxio@l7#on4rg&xnvw=nz#u=iw7$^k)qr+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
@@ -78,11 +79,15 @@ WSGI_APPLICATION = 'ehspathway.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
+import dj_database_url
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'default': dj_database_url.config(
+                default='sqlite:///db.sqlite3', # Fallback to local SQLite
+                conn_max_age=600
+            )
     }
 }
 
@@ -121,8 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
 
-STATIC_ROOT = BASE_DIR/'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
